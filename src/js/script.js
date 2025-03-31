1eaee96e28a903ba9956a3b3f9b215fa1d6c1d9f
Matheus <matheusbispo0404@gmail.com>
2025-03-31 09:07:49 -0300

    declaracao e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome="fiap";
+console.log(nome);
+
+let idade =19;
+console.log(idade);
+
+const sobrenome="Fiapinho"
+console.log(sobrenome);
+
+//declara ndo uma variavel indefinida
+let nome1;
+console.log(nome1);
+
+//declaração de uma variavel nula
+let nome2=null
+console.log(nome2);
+
+
